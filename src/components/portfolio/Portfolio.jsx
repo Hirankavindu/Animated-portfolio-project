2fdc1a959e25d60620a6b3cd4cf8880b48afe570
Hiran Kavindu Vishmith <97301123+Hirankavindu@users.noreply.github.com>
2023-11-01 02:28:09 +0530

    complete portfolio section with motion animations

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,87 @@
+import { useRef } from "react";
+import "./portfolio.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+const items = [
+  {
+    id: 1,
+    title: "React Commerce",
+    img: "/public/pr1.png",
+    desc: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+  },
+  {
+    id: 2,
+    title: "Next.js Commerce",
+    img: "/public//pr2.jpeg",
+    desc: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+  },
+  {
+    id: 3,
+    title: "React Native Commerce",
+    img: "https://img.freepik.com/free-vector/hand-drawn-dia-de-muertos-background_52683-46544.jpg?w=2000&t=st=1698783905~exp=1698784505~hmac=7d37fc1e575e87e24eeb7194453722587cfcfc538ba22bed64279cfa43ad2a07",
+    desc: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+  },
+  {
+    id: 4,
+    title: "Flutter Commerce",
+    img: "/public/pr4.jpeg",
+    desc: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+  },
+];
+
+const Single = ({ item }) => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    // offset:["start start", "end start"]
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-200, 200]);
+  return (
+    <section>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>See Demo</button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+function Portfolio() {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Featured Works</h1>
+        <motion.div
+          style={{ scaleX: scaleX }}
+          className="progressBar"
+        ></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
+}
+
+export default Portfolio;
