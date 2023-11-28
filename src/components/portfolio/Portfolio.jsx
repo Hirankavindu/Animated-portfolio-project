5e46c4a50ff11593a59410d49155af872b34a8ed
Hiran Kavindu Vishmith <97301123+Hirankavindu@users.noreply.github.com>
2023-11-28 07:22:31 +0530

    update the path of the image

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,25 +6,25 @@ const items = [
   {
     id: 1,
     title: "Movie Application",
-    img: "/public/project1.png",
+    img: "/project1.png",
     desc: "I have developed a mobile application using react native and using TMDB. By using this app we can see the details of the trending movies, Top rated Movies, Acters names and details. This is a cross Platform application. ",
   },
   {
     id: 2,
     title: "NAPOT Application",
-    img: "/public/project3.png",
+    img: "/project3.png",
     desc: "I have developed a mobile application called NAPOT for NSBM Green University. I have developed this application using Flutter and Firebase. By using this app NSBM students, Lecturers, Librarian, Canteen Admin, Interviewer can do their day to day works easily.",
   },
   {
     id: 3,
     title: "Intergalactic Application",
-    img: "/public/project2.png",
+    img: "/project2.png",
     desc: "We have develop this application Using Next.Js and MongoDB We develop this application for Tech-Triathlon Competition that Organized by RootCode. This application designs as a featuristic design. WE have got the Second Runner Up place in this competition.",
   },
   {
     id: 4,
     title: "KentoVet Application",
-    img: "/public/project4.png",
+    img: "/project4.png",
     desc: "I have develop this application using C#, Guna Frame Work and PL SQL. We develop this application for KentoVet Pharmaceutical pvt.LTD for manage their stocks. By using this application they can prediction about future sales and get a clear idea about the stocks.",
   },
 ];
